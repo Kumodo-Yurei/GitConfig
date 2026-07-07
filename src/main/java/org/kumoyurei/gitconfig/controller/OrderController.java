c6c694a91873f08595f917fc445a25104fa6c07f
Kumodo-Yurei <tanakashioji@163.com>
2026-07-07 16:16:48 +0800

    初始化控制器

---
+++ src/main/java/org/kumoyurei/gitconfig/controller/OrderController.java
@@ -0,0 +1,10 @@
+package org.kumoyurei.gitconfig.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+
+}
